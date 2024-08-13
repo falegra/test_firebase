9a510b5499844ba7e4a007708efc6392877ed656
falegra <fidellegra60@gmail.com>
2024-08-13 00:17:06 -0400

    deleted app.controller and app.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FirebaseModule } from './firebase/firebase.module';
 import { UserModule } from './user/user.module';
 import { HelpersModule } from './helpers/helpers.module';
@@ -15,7 +13,7 @@ import { AuthModule } from './auth/auth.module';
     HelpersModule,
     AuthModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
